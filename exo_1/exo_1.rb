d481f51b280339b4ee349e7098eb2ca0f6b50594
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-11-11 22:26:35 +0100

    exo 1 ok

---
+++ exo_1/exo_1.rb
@@ -0,0 +1,17 @@
+# ## Exo 1 - Le palindrone
+# Le script Ruby permettra de saisir un mot et de vérifier que ce mot est un palindrome.
+# Le retour de code se fera comme suit :
+# Le mot mot saisie est un palindrome
+# Le mot mot saisie n’est pas un palindrome
+
+# je prepare la methode palindrome qui attend une string
+def palindrome(string)
+    # si la string est egal a la sring inverser
+    if string == string.reverse
+        # la string est bien un palindrome 
+      return "le mot saisie est un palindrome"
+    else
+        # sinon la string n'est pas un palindrome 
+      puts "Le mot mot saisie n’est pas un palindrome"
+    end
+   end
